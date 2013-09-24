9886cf9a4551d15128e263922bafe7a66523ab7a
Eszter Legar <Eszter_Legar@epam.com>
2013-09-24 11:27:54 +0200

    web.xml to JavaConfig - step1

---
+++ src/main/java/hu/legare/contextconfig/config/AnnotationDrivenWebApplicationInitializer.java
@@ -0,0 +1,36 @@
+package hu.legare.contextconfig.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+import javax.servlet.http.HttpServlet;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class AnnotationDrivenWebApplicationInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext container) {
+        // Create the 'root' Spring application context
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+        rootContext.register(ApplicationContext.class);
+
+        // Manage the lifecycle of the root application context
+        container.addListener(new ContextLoaderListener(rootContext));
+
+        // Create the dispatcher servlet's Spring application context
+        AnnotationConfigWebApplicationContext dispatcherContext = new AnnotationConfigWebApplicationContext();
+        dispatcherContext.register(DispatcherServletContext.class);
+
+        // Register and map the dispatcher servlet
+        ServletRegistration.Dynamic dispatcher = container.addServlet("dispatcherServlet", new DispatcherServlet(dispatcherContext));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+
+        ServletRegistration.Dynamic htmlJspMapper = container.addServlet("jsp", );
+        htmlJspMapper.setLoadOnStartup(1);
+        htmlJspMapper.addMapping("*.html");
+    }
+}
